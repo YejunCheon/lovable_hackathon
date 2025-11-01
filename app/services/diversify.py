e269b7c3e8d0b2f18f026896ce990bd072e02cad
rycont <reactdev@kakao.com>
2025-11-02 06:14:02 +0900

    Refactor code structure for improved readability and maintainability

--- app/services/diversify.py
+++ app/services/diversify.py
@@ -19,8 +19,18 @@ def diversify_search_query(search_query: str) -> List[str]:
             "thinkingConfig": {"thinking_budget": 0},
             "response_mime_type": "application/json",
             "response_schema": Response,
-            "system_instruction": "Diversify following search query into specific word-level queries, each query should be 2~3 words long. Create 12 queries."
+            "system_instruction": """
+Diversify following search query into specific word-level queries, each query should be 2~3 words long. Create 10 various and dynamic queries.
+
+Example1: 클라우드 개발 해본 사람 없나
+Output: ['AWS 개발 경험', '클라우드 엔지니어', '백엔드 개발', '클라우드 아키텍트', 'DevOps', '인프라 엔지니어', 'SRE 운영', '마이크로서비스 아키텍처', '서버리스 개발']
+
+Example2: 커피 좋아하는 사람
+Output: ['커피 취미', '스페셜티 로스팅', '카페 운영', '커피 소믈리에', '바리스타 경력', '에스프레소 머신', '핸드드립', '홈카페', '로스팅 클래스']
+"""
         }
     )
+
+    print("Diversified", response.parsed.search_queries)
 
     return response.parsed.search_queries
--- app/services/search_profile_with_vectors.py
+++ app/services/search_profile_with_vectors.py
@@ -4,17 +4,17 @@ from app.adapters.pg import execute_query
 
 
 async def search_profile_with_vectors(vectors: List[List[float]]) -> List[str]:
-    vector_search_sum_equation = " + ".join([f"(vector <=> '{vector}'::vector)" for vector in vectors])
+    vector_search_sum_equation = ", ".join([f"(vector <-> '{vector}'::vector)" for vector in vectors])
     query = f"""
     WITH vector_search AS (
-        SELECT ({vector_search_sum_equation}) AS score, text FROM text_embeddings
+        SELECT LEAST({vector_search_sum_equation}) AS distance, text FROM text_embeddings
     ),
     searched_candidate AS (
-        SELECT candidate_id, sum(score) as score_sum FROM candidate_embedding_word_map
+        SELECT candidate_id, min(distance) as distance_min FROM candidate_embedding_word_map
         INNER JOIN vector_search ON candidate_embedding_word_map.text = vector_search.text
         GROUP BY candidate_id
     )
-    SELECT candidate_id FROM searched_candidate ORDER BY score_sum DESC LIMIT 10
+    SELECT candidate_id FROM searched_candidate ORDER BY distance_min LIMIT 10
     """
     
     with open("query.sql", "w") as f:
